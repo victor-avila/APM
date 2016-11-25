a29e66a280f971dbb8bfb1691db4b2d0c4c40368
vicavila <idoxobi@gmail.com>
2016-11-25 13:44:35 -0500

    Interfaces, life cycle and custom pipes
    Encapsulating component styles, defining relative paths within
    components

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product'
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    moduleId: module.id,
+    templateUrl: 'product-list.component.html',
+    styleUrls: [ 'product-list.component.css' ]
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -35,5 +38,8 @@ export class ProductListComponent {
     ];
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+    ngOnInit(): void {
+        console.log('In OnInit');
     }
 }
